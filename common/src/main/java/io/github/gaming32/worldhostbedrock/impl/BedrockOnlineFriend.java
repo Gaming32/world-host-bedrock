d23af445bccb6feaf5f8c2c28de6c144747caebc
Josiah Glosson <soujournme@gmail.com>
2024-07-16 17:06:29 -0500

    Show a join warning about ViaBedrock being in alpha

--- common/src/main/java/io/github/gaming32/worldhostbedrock/impl/BedrockOnlineFriend.java
+++ common/src/main/java/io/github/gaming32/worldhostbedrock/impl/BedrockOnlineFriend.java
@@ -1,6 +1,7 @@
 package io.github.gaming32.worldhostbedrock.impl;
 
 import io.github.gaming32.worldhost.WorldHost;
+import io.github.gaming32.worldhost.plugin.Joinability;
 import io.github.gaming32.worldhost.plugin.OnlineFriend;
 import io.github.gaming32.worldhostbedrock.connect.BedrockConnection;
 import io.github.gaming32.worldhostbedrock.util.XUID;
@@ -9,7 +10,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public record BedrockOnlineFriend(
@@ -42,16 +42,16 @@ public record BedrockOnlineFriend(
     }
 
     @Override
-    public Optional<Component> unjoinableReason() {
-        if (connection == null) {
-            return Optional.of(Component.translatable("world_host_bedrock.join.no_methods"));
-        }
+    public Joinability joinability() {
         if (!WorldHost.isModLoaded("viafabricplus")) {
-            return Optional.of(Component.translatable("world_host_bedrock.join.no_vfp"));
+            return new Joinability.Unjoinable(Component.translatable("world_host_bedrock.join.no_vfp"));
+        }
+        if (connection == null) {
+            return new Joinability.Unjoinable(Component.translatable("world_host_bedrock.join.no_methods"));
         }
         if (connection instanceof BedrockConnection.Unsupported unsupported) {
-            return Optional.of(Component.literal(unsupported.reason()));
+            return new Joinability.Unjoinable(Component.literal(unsupported.reason()));
         }
-        return Optional.empty();
+        return new Joinability.JoinableWithWarning(Component.translatable("world_host_bedrock.join.vb_in_alpha"));
     }
 }
